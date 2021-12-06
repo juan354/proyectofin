73292dee14d623ddddbfe67080d3c1af930e0909
juan354 <58792413+juan354@users.noreply.github.com>
2021-12-05 21:25:48 -0600

    SEGUNDA PRUEBA

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  apiKey: string = 'fdf4b68ea3f26b28aae93619129910e7';
+  URI: string = '';
+
+  constructor(private http: HttpClient) {
+    this.URI = `https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&units=metric&q=`;
+  }
+
+  getWeather(cityName: string, countryCode: string) {
+    return this.http.get(`${this.URI}${cityName},${countryCode}`);
+  }
 }
